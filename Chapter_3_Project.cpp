5f67c2df6b6dd06af36f5983e6f6d533bc6dfdc3
Joshua Brunson <joshuarbrunson@gmail.com>
2025-03-05 20:16:35 -0500

    Math_Tutor Complete

--- Chapter_3_Project.cpp
+++ Chapter_3_Project.cpp
@@ -2,10 +2,33 @@
 //
 
 #include <iostream>
+#include <random>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int FirstRandomNumber; //initialize first random number
+    int SecondRandomNumber; //initialize second random number
+    int Sum; //initialize sum
+
+    random_device engineOne; //random number generator engine
+
+    uniform_int_distribution<int> Number(1, 500); //random number object
+  
+    FirstRandomNumber = Number(engineOne); // Randomly generate first number
+    SecondRandomNumber = Number(engineOne); //Randomly generate second number
+    
+    Sum = FirstRandomNumber + SecondRandomNumber; //Store the sum of first and second number in "Sum"
+
+   
+    cout << "What is the sum of " << FirstRandomNumber << " and " << SecondRandomNumber << "?" << endl; //Prompt user to add first and second and number
+    cout << "Enter any key when you are ready for the answer.  "; //Prompt user to press any key to see the answer
+    cin.get(); //Pause screen until any key is entered
+    cout << endl;
+    cout << "The correct answer is " << Sum << "." << endl; //Print the correct answer
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
