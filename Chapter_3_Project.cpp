cbca47868a17d90ade34102453531d16afa56915
Joshua Brunson <joshuarbrunson@gmail.com>
2025-03-05 21:08:39 -0500

    Interest_Earned Complete

--- Chapter_3_Project.cpp
+++ Chapter_3_Project.cpp
@@ -2,10 +2,45 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double Principal; // initialize Principle
+    double InterestRate; // initialize interest rate
+    int TimesCompounded; // Initialize the T
+    double Parentheses; // Initialize parentheses
+    double Exponent; // Initialize Exponent
+    double Interest; // Initialize Interest
+    double Amount; // initialize amount
+
+    cout << "Enter the Principal: "; //Prompt user for Principal
+    cin >> Principal;
+    cout << "Enter the Interest Rate (%): "; //Prompt user for the Interest Rate
+    cin >> InterestRate;
+    InterestRate = InterestRate / 100; //convert percentage to correct number
+    cout << "Enter the annual number of times that the interest is compounded: "; //Prompt user for T
+    cin >> TimesCompounded;
+
+    
+    Parentheses = 1 + (InterestRate / TimesCompounded); //Compute Parentheses // (1 + (Rate/T))
+    Exponent = pow(Parentheses, TimesCompounded); // Compute Exponent using a power function of parentheses raised to T
+    Amount = Principal * Exponent; //Compute Amount
+
+    Interest = Amount - Principal; //Cpmpute Interest = Amount - Principal
+
+    InterestRate = InterestRate * 100; //Convert back to percentage
+
+    cout << "Interest Rate:" << setw(23) << setprecision(2) << fixed << InterestRate << "%" << endl; //Print Interest Rate with proper alignment and decimal
+    cout << "Times Compounded:" << setw(20) << setprecision(2) << fixed << TimesCompounded << endl; //Print Interest with proper alignment and decimal places
+    cout << "Principal:" << setw(20) << "$ " << setprecision(2) << fixed << Principal << endl; // Print Principal with proper alignment and decimal places
+    cout << "Interest:" << setw(21) << "$ " << setw(7) << setprecision(2) << fixed << Interest << endl; //Print Interest with proper alignment and decimal places
+    cout << "Amount in Savings:" << setw(12) << "$ " << setprecision(2) << fixed << Amount << endl; //Print Amount in Savings with proper alignment and decimal places
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
