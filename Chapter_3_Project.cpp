6a8c1edd8c803998bcbd888dced906c117e6e173
Joshua Brunson <joshuarbrunson@gmail.com>
2025-03-05 19:25:47 -0500

    Ingredient_Adjuster is complete

--- Chapter_3_Project.cpp
+++ Chapter_3_Project.cpp
@@ -1,11 +1,35 @@
 // Chapter_3_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* This program calculate the amounts of each ingredient needed to produce a number of cookies
+that are defined by user input*/
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double SugarRatio = 1.5 / 48; //Sugar required to produce one cookie
+    double ButterRatio = 1.0 / 48; //Butter required to produce one cookie
+    double FlourRatio = 2.75 / 48; //Flour required to produce one cookie
+    int Cookies; //Initialize Cookies
+    double CupsOfSugar; // initialize cups of sugar
+    double CupsOfButter; // initialize cups of butter
+    double CupsOfFlour; // initialize cups of flour
+    
+    cout << "Enter the desired numbe of cookies "; //Prompts for user input
+    cin >> Cookies; // Get number of cookies from the user
+    cout << endl;
+
+    CupsOfSugar = Cookies * SugarRatio; // compute sugar required
+    CupsOfButter = Cookies * ButterRatio; // compute butter required
+    CupsOfFlour = Cookies * FlourRatio; // compute flour required
+
+    cout << setprecision(3) << CupsOfSugar << " cups of sugar are needed for " << Cookies << " cookies." << endl; //compute sugar required
+    cout << setprecision(3) << CupsOfButter << " cups of butter are needed for " << Cookies << " cookies." << endl; //compute butter required
+    cout << setprecision(3) << CupsOfFlour << " cups of flour are needed for " << Cookies << " cookies." << endl; //compute flour required
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
